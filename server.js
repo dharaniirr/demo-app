f8c38e76eb077d710c66205ae4e47f77d68bcb14
dharaniirr <dharaniirr@gmail.com>
2025-12-06 13:25:55 -0600

    Update server.js
    APP VALIDATION CHECK

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("CI/CD 🚀 Deployed from GitHub Actions!"));
+app.get("/", (req, res) => res.send("CI/CD Pipeline Update v2 — LIVE! 🔥
+"));
 app.get("/healthz", (req, res) => res.send("ok"));
 
 app.listen(port, () =>
