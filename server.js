03d3699aa4097e4debee82d47bde851aa0b5dcf2
Dharani Ramakrishnan <dharaniramakrishnan@Mac.lan>
2025-12-05 19:16:55 -0600

    Intial working app

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("CI/CD 🚀 Deployed from GitHub Actions!"));
+app.get("/healthz", (req, res) => res.send("ok"));
+
+app.listen(port, () =>
+  console.log(`Running at http://localhost:${port}`)
+);
+
